063ba6f6227204de07139531dbf9b0be7176c67d
Samuel Galonska <samuelGalonska@protonmail.com>
2023-11-30 16:44:09 +0100

    New Firefly System

--- Assets/Scripts/Firefly/FireflySpawner.cs
+++ Assets/Scripts/Firefly/FireflySpawner.cs
@@ -1,35 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class FireflySpawner : MonoBehaviour
 {
     [SerializeField] FireflyWalk fireflyPrefab;
     [SerializeField] int ringCount;
-    [SerializeField] float worldRadius;
+    [SerializeField] float outerRadius;
     [SerializeField] float innerRadius;
+
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(Vector3.zero, innerRadius);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(Vector3.zero, outerRadius);
+    }
 
     void Start()
     {
+        float halfRingWidth = (outerRadius - innerRadius) / ringCount / 2;
+
+        //for each ring
         for(int i = 0; i < ringCount; i++)
         {
-            float centerDistance = ((worldRadius - innerRadius) / ringCount * i) + innerRadius;
+            float centerDistance = ((outerRadius - innerRadius) / ringCount * i) + innerRadius + halfRingWidth;
 
-            Vector3 startFireflyPosition = Vector3.right * centerDistance;
+            Vector3 fireflyStartPosition = Vector3.right * centerDistance;
             float randomRotation = Random.Range(0f, 360f);
-            int currentRingFireflyAmount = i + 1;
-            float rotationPerFirefly = 360f / currentRingFireflyAmount;
+            int fireflyAmountInRing = (int)Mathf.Pow(2, i + 2);
+            float rotationPerFirefly = 360f / fireflyAmountInRing;
 
-            for (int o = 0; o < currentRingFireflyAmount; o++)
+            //for each firefly inside the ring
+            for (int o = 0; o < fireflyAmountInRing; o++)
             {
                 float fireflyRotation = randomRotation + rotationPerFirefly * o;
-                Vector3 fireflyPosition = Quaternion.Euler(0, fireflyRotation, 0) * startFireflyPosition;
+                Vector3 fireflyPosition = Quaternion.Euler(0, fireflyRotation, 0) * fireflyStartPosition;
                 FireflyWalk currentFirefly = Instantiate(fireflyPrefab, fireflyPosition, Quaternion.identity);
 
-                if(i%2 == 0)
-                {
-                    currentFirefly.SetDirection(false);
-                }
+                bool moveRight = i % 2 == 0;
+                currentFirefly.SetFireflyValues(halfRingWidth / 2, moveRight);
+
+                currentFirefly.transform.parent = transform;
             }
         }         
     }
--- Assets/Scripts/Firefly/FireflyWalk.cs
+++ Assets/Scripts/Firefly/FireflyWalk.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.AI;
@@ -6,55 +7,60 @@ using UnityEngine;
 
 public class FireflyWalk : MonoBehaviour
 {
-    private NavMeshAgent navMeshAgent;
-    [SerializeField] private Transform destinationPoint;
+    [Header("Variables")]
     [SerializeField] float speed;
-
+    [SerializeField] float timeToRespawn;
+    [SerializeField] private float collectSpeed;
+    
+    private Vector3 rotateAroundPosition;
+    private float walkCircleRadius;
+    private float destinationPointSpeed;
+    private bool moveRight = true;
+    
+    [Header("References")]
+    [SerializeField] private Transform destinationPoint;
     [SerializeField] Transform visuals;
     [SerializeField] SplineAnimate spline;
-
-    [SerializeField] float timeToRespawn;
-
-    [SerializeField] private float collectSpeed;
-
-    private Collider _collider;
-
-    float destinationPointSpeed;    
-    float centerDistance;
-
-    bool moveRight = true;
-
-    Vector3 startPosition; 
     
+    private NavMeshAgent navMeshAgent;
+    private Collider sphereCollider;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(rotateAroundPosition, Vector3.up);
+    }
+
     void Start()
     {
+        destinationPoint.parent = transform.parent;
         navMeshAgent = GetComponent<NavMeshAgent>();
-        navMeshAgent.speed = speed * 2;
-        navMeshAgent.acceleration = speed * 2;
+        sphereCollider = GetComponent<SphereCollider>();
 
-        centerDistance = Vector3.Distance(Vector3.zero, transform.position);
-        speed /= centerDistance;
-        speed *= 50;
+        navMeshAgent.nextPosition = destinationPoint.position;
+        navMeshAgent.speed = speed;
+        navMeshAgent.acceleration = speed;
 
-        destinationPointSpeed = moveRight ? speed : -speed;
-
-        startPosition = transform.position;
-
-        _collider = GetComponent<SphereCollider>();
+        
     }
 
     
     void Update()
     {
-        Quaternion destinationPointRotation = Quaternion.Euler(0, Time.time * destinationPointSpeed, 0);
-        Vector3 currentDestination = destinationPointRotation * startPosition;
+        destinationPoint.RotateAround(rotateAroundPosition, Vector3.up, destinationPointSpeed * Time.deltaTime);
 
-        navMeshAgent.SetDestination(destinationPoint.position = currentDestination);
+        navMeshAgent.SetDestination(destinationPoint.position);
     }
 
-    public void SetDirection (bool moveRight)
+    public void SetFireflyValues(float walkCircleRadius, bool moveRight)
     {
         this.moveRight = moveRight;
+        this.walkCircleRadius = walkCircleRadius;
+        
+        rotateAroundPosition = transform.position;
+        destinationPoint.position = rotateAroundPosition + Vector3.right * this.walkCircleRadius;
+
+        destinationPointSpeed = speed / walkCircleRadius * 10;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -62,41 +68,34 @@ public class FireflyWalk : MonoBehaviour
         if (other.tag.Equals("FireflyCollector"))
         {
             spline.enabled = false;
-            _collider.enabled = false;
+            sphereCollider.enabled = false;
             StartCoroutine(MoveToPlayer(other.transform));
         }
     }
-
     IEnumerator MoveToPlayer(Transform player)
     {
         Vector3 startPos = visuals.position;
         float t = 0;
-
+    
         while (t < 1)
         {
             t += Time.deltaTime * collectSpeed;
             visuals.position = Vector3.Lerp(startPos, player.position, t * t);
             yield return null;
         }
-
+    
         player.parent.GetComponent<PlayerController>().CollectFirefly();
-
+    
         visuals.gameObject.SetActive(false);
-
+    
         StartCoroutine(WaitTillRespawn());
     }
-
+    
     IEnumerator WaitTillRespawn()
     {
-        float TimeElapsed = 0;
-
-        while (TimeElapsed < timeToRespawn)
-        {
-            TimeElapsed += Time.deltaTime;
-            yield return null;
-        }
-
-        _collider.enabled = true;
+        yield return new WaitForSeconds(timeToRespawn);
+    
+        sphereCollider.enabled = true;
         spline.enabled = true;
         visuals.gameObject.SetActive(true);
     }
