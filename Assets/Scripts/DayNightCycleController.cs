0a500d92ada14dfcfeb99e9dd95016033e0df73d
Samuel Galonska <samuelGalonska@protonmail.com>
2023-12-12 16:20:28 +0100

    New DayNightCycle System with Lerps

--- Assets/Scripts/DayNightCycleController.cs
+++ Assets/Scripts/DayNightCycleController.cs
@@ -1,83 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class DayNightCycleController : MonoBehaviour
 {
-    [Range(0, 24)] //24 Hours Cycle
-    [SerializeField] float orbitSpeed = 1.0f;
-    public float timeOfDay;
-    public Light moon;
-    private bool isNight;
+    [SerializeField] private float sunStartRotation;
+    [SerializeField] private float sunDestinationRotation;
+    [SerializeField] private float moonStartRotation;
+    [SerializeField] private float moonDestinationRotation;
+    
+    // private bool isNight;
+    public float gameTime = 60;
 
-    public Light sun1;
-    public Light sun2;
+    [SerializeField] private Light sun;
+    [SerializeField] private Light moon;
+    
     private void Update()
     {
-        timeOfDay += Time.deltaTime * orbitSpeed;
-        if (timeOfDay > 24)
-            timeOfDay = 0;
         UpdateTime();
     }
-    private void OnValidate()
-    {
-        UpdateTime();
-    }
-    private void Start()
-    {
-        sun2.enabled = false;
-        StartDay1();
-    }
+
     private void UpdateTime()
     {
-        float alpha = timeOfDay / 24.0f;
-        float sun1Rotation = Mathf.Lerp(-90, 270, alpha);
-        float moonRotation = sun1Rotation - 130;
-        float sun2Rotation = sun1Rotation + 60;
-        sun1.transform.rotation = Quaternion.Euler(sun1Rotation, 0, 0);
-        sun2.transform.rotation = Quaternion.Euler(sun2Rotation, 0, 0);
+        float t = Mathf.Min(Time.time / gameTime, 1);
+        
+        float sunRotation = Mathf.Lerp(sunStartRotation, sunDestinationRotation, t);
+        float moonRotation = Mathf.Lerp(moonStartRotation, moonDestinationRotation, t);
+        
+        sun.transform.rotation = Quaternion.Euler(sunRotation, 0, 0);
         moon.transform.rotation = Quaternion.Euler(moonRotation, 0, 0);
-
-        CheckNightDayTransition();
     }
-    private void CheckNightDayTransition()
-    {
-        if (isNight)
-        {
-            if(moon.transform.rotation.eulerAngles.x > 180)
-            {
-                StartDay2();
-            }
-        }
-        else
-        {
-            if (sun1.transform.rotation.eulerAngles.x > 180)
-            {
-                StartNight();
-            }
-        }
-    }
-    private void StartDay1()
-    {
-        isNight = false;
-        sun1.shadows = LightShadows.Soft;
-        //sun2.shadows = LightShadows.None;
-        moon.shadows = LightShadows.None;
-    }
-    private void StartNight ()
-    {
-        isNight = true;
-        sun2.enabled = true;
-        moon.shadows = LightShadows.Soft;
-        sun1.shadows = LightShadows.None;
-        sun2.shadows = LightShadows.None;
-    }
-    private void StartDay2 ()
-    {
-        isNight = false;
-        sun1.enabled = false;
-        sun2.shadows = LightShadows.Soft;
-        //sun1.shadows = LightShadows.None;
-        moon.shadows = LightShadows.None;
-    }
+    // private void CheckNightDayTransition()
+    // {
+    //     if (sun1.transform.eulerAngles.x > 180 && !isNight && sun1.enabled)
+    //     {
+    //         StartNight();
+    //     }
+    //     else if (moon.transform.eulerAngles.x > 180 && isNight)
+    //     {
+    //         StartDay2();
+    //     }
+    // }
+    // private void StartDay1()
+    // {
+    //     Debug.Log("StartDay 1");
+    //     isNight = false;
+    //     sun1.shadows = LightShadows.Soft;
+    //     moon.shadows = LightShadows.None;
+    // }
+    // private void StartNight ()
+    // {
+    //     Debug.Log("Start Night");
+    //     isNight = true;
+    //     sun2.enabled = true;
+    //     sun1.enabled = false;
+    //     moon.shadows = LightShadows.Soft;
+    //     sun1.shadows = LightShadows.None;
+    //     sun2.shadows = LightShadows.None;
+    // }
+    // private void StartDay2 ()
+    // {
+    //     Debug.Log("Start Day 2");
+    //     isNight = false;
+    //     sun2.shadows = LightShadows.Soft;
+    //     moon.shadows = LightShadows.None;
+    // }
 }
