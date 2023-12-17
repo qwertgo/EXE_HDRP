8b61642e3c77ed8d9e3aaf41d09e64b2a5c289a7
Bausparverquarkt <s0586433@htw-berlin.de>
2023-12-17 23:37:01 +0100

    Added 2 new Wood Waterials, added Sky Color Lerp

--- Assets/Scripts/DayNightCycleController.cs
+++ Assets/Scripts/DayNightCycleController.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.Serialization;
 
 public class DayNightCycleController : MonoBehaviour
@@ -10,13 +13,25 @@ public class DayNightCycleController : MonoBehaviour
     [SerializeField] private float sunDestinationRotation;
     [SerializeField] private float moonStartRotation;
     [SerializeField] private float moonDestinationRotation;
-    
+
+    [SerializeField] private Color startColor;
+    [SerializeField] private Color endColor;
+
+    /*[SerializeField] private Volume skyAndFogVolume;
+    private Volume globalVolume;
+    [SerializeField] private float startFogDistance = 20f;
+    [SerializeField] private float endFogDistance = 1f;*/
+
     // private bool isNight;
     public float gameTime = 60;
 
     [SerializeField] private Light sun;
     [SerializeField] private Light moon;
-    
+
+    private void Awake()
+    {
+        //globalVolume = skyAndFogVolume.GetComponent<Volume>();
+    }
     private void Update()
     {
         UpdateTime();
@@ -25,13 +40,25 @@ public class DayNightCycleController : MonoBehaviour
     private void UpdateTime()
     {
         float t = Mathf.Min(Time.time / gameTime, 1);
-        
+
+        Color lerpedColor = Color.Lerp(startColor, endColor, t);
+        moon.color = lerpedColor;
+
+        /*float lerpedFogDistance = Mathf.Lerp(startFogDistance, endFogDistance, t);
+        HDRISky hDRISky;
+        if (globalVolume.profile.TryGet(out hDRISky))
+        {
+            hDRISky.groundLevel.value = lerpedFogDistance;
+        }*/
+
+
         float sunRotation = Mathf.Lerp(sunStartRotation, sunDestinationRotation, t);
         float moonRotation = Mathf.Lerp(moonStartRotation, moonDestinationRotation, t);
-        
+
         sun.transform.rotation = Quaternion.Euler(sunRotation, 0, 0);
         moon.transform.rotation = Quaternion.Euler(moonRotation, 0, 0);
     }
+
     // private void CheckNightDayTransition()
     // {
     //     if (sun1.transform.eulerAngles.x > 180 && !isNight && sun1.enabled)
@@ -68,3 +95,77 @@ public class DayNightCycleController : MonoBehaviour
     //     moon.shadows = LightShadows.None;
     // }
 }
+
+/*using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class DayNightCycleController : MonoBehaviour
+{
+    [SerializeField] private float sunStartRotation;
+    [SerializeField] private float sunDestinationRotation;
+    [SerializeField] private float moonStartRotation;
+    [SerializeField] private float moonDestinationRotation;
+    
+    // private bool isNight;
+    public float gameTime = 60;
+
+    [SerializeField] private Light sun;
+    [SerializeField] private Light moon;
+    
+    private void Update()
+    {
+        UpdateTime();
+    }
+
+    private void UpdateTime()
+    {
+        float t = Mathf.Min(Time.time / gameTime, 1);
+        
+        float sunRotation = Mathf.Lerp(sunStartRotation, sunDestinationRotation, t);
+        float moonRotation = Mathf.Lerp(moonStartRotation, moonDestinationRotation, t);
+        
+        sun.transform.rotation = Quaternion.Euler(sunRotation, 0, 0);
+        moon.transform.rotation = Quaternion.Euler(moonRotation, 0, 0);
+    }
+    
+    
+    // private void CheckNightDayTransition()
+    // {
+    //     if (sun1.transform.eulerAngles.x > 180 && !isNight && sun1.enabled)
+    //     {
+    //         StartNight();
+    //     }
+    //     else if (moon.transform.eulerAngles.x > 180 && isNight)
+    //     {
+    //         StartDay2();
+    //     }
+    // }
+    // private void StartDay1()
+    // {
+    //     Debug.Log("StartDay 1");
+    //     isNight = false;
+    //     sun1.shadows = LightShadows.Soft;
+    //     moon.shadows = LightShadows.None;
+    // }
+    // private void StartNight ()
+    // {
+    //     Debug.Log("Start Night");
+    //     isNight = true;
+    //     sun2.enabled = true;
+    //     sun1.enabled = false;
+    //     moon.shadows = LightShadows.Soft;
+    //     sun1.shadows = LightShadows.None;
+    //     sun2.shadows = LightShadows.None;
+    // }
+    // private void StartDay2 ()
+    // {
+    //     Debug.Log("Start Day 2");
+    //     isNight = false;
+    //     sun2.shadows = LightShadows.Soft;
+    //     moon.shadows = LightShadows.None;
+    // }
+}
+*/
