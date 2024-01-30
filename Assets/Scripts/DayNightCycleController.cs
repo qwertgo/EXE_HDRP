fe4d9c58bda8af62a06c39d219e8ae5f6d4acd5e
kiraajasmin <148049876+kiraajasmin@users.noreply.github.com>
2024-01-30 14:23:50 +0100

    Pretty schrift

--- Assets/Scripts/DayNightCycleController.cs
+++ Assets/Scripts/DayNightCycleController.cs
@@ -7,12 +7,13 @@ using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.Serialization;
 
+
 public class DayNightCycleController : MonoBehaviour
 {
     [SerializeField] private float timeForSunToSet = 10;
     private float sunsetSpeed;
     private float sunStartIntensity;
-    
+
     [SerializeField] private float sunDestinationRotation;
     [SerializeField] private float moonDestinationRotation;
     private Quaternion sunStartRotation;
@@ -20,23 +21,28 @@ public class DayNightCycleController : MonoBehaviour
     private Quaternion moonStartRotation;
     private Quaternion moonEndRotation;
 
-    [FormerlySerializedAs("startColor")] [SerializeField] private Color moonColorStart;
-    [FormerlySerializedAs("endColor")] [SerializeField] private Color moonColorEnd;
+    [FormerlySerializedAs("startColor")][SerializeField] private Color moonColorStart;
+    [FormerlySerializedAs("endColor")][SerializeField] private Color moonColorEnd;
 
     [SerializeField] private Light sun;
     [SerializeField] private Light moon;
+
 
     private void Start()
     {
         sunStartRotation = sun.transform.rotation;
         sunEndRotation = Quaternion.Euler(sunDestinationRotation, 0, 0);
-        
+
         moonStartRotation = moon.transform.rotation;
         moonEndRotation = Quaternion.Euler(moonDestinationRotation, 0, 0);
-        
+
         sunsetSpeed = 1 / timeForSunToSet;
         sunStartIntensity = sun.intensity;
         StartCoroutine(Sunset());
+
+       
+
+
     }
 
     IEnumerator Sunset()
@@ -49,7 +55,7 @@ public class DayNightCycleController : MonoBehaviour
             SetSunsetValues(t);
             yield return null;
         }
-        
+
         SetSunsetValues(1);
     }
 
@@ -57,7 +63,7 @@ public class DayNightCycleController : MonoBehaviour
     {
         Color lerpedColor = Color.Lerp(moonColorStart, moonColorEnd, t);
         moon.color = lerpedColor;
-        
+
         float sunIntensityT = (t - .5f) * 2;
         sunIntensityT = Mathf.Clamp01(sunIntensityT);
         sun.intensity = Mathf.Lerp(sunStartIntensity, 0, sunIntensityT);
@@ -65,4 +71,6 @@ public class DayNightCycleController : MonoBehaviour
         sun.transform.rotation = Quaternion.Lerp(sunStartRotation, sunEndRotation, t);
         moon.transform.rotation = Quaternion.Lerp(moonStartRotation, moonEndRotation, t);
     }
+
+    
 }
